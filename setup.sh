7eb3b3a326e1fc51014e04d0520df4884a099ca2
Gaku Ueda <gaku@gaku.net>
2017-02-12 08:37:47 +0000

    added the initial setup script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# install git
+apt-get update
+apt-get install -y git
+apt-get install -y nodejs
+apt-get install -y vim
+# install heroku cli
+add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
+curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+apt-get install heroku
