d98063c7f09e1298d92caf9ba40fd53dbf9e2006
Gaku Ueda <gaku@tinkermode.com>
2017-02-17 11:19:14 +0900

    apt-get update was mandatory

--- setup.sh
+++ setup.sh
@@ -8,4 +8,5 @@ apt-get install -y vim
 # install heroku cli
 add-apt-repository "deb https://cli-assets.heroku.com/branches/stable/apt ./"
 curl -L https://cli-assets.heroku.com/apt/release.key | sudo apt-key add -
+apt-get update
 apt-get install heroku
